ef614aba69b4f21eaa4a2dc76f4bd77f54618801
Brian <97054982+reganbrianpatrick@users.noreply.github.com>
2025-03-13 15:42:09 -0400

    test
    test for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v1 App',
+  title: 'v1000 App',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
